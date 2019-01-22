7a86420108ec6fc06d8593252fa0596353b4e0ee
Daniel Kornas <harvi_pl@wp.pl>
2019-01-22 14:44:52 +0100

    New components to show basic app logic, new css module

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>Burger</div>
+        <div>Burger controls</div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default BurgerBuilder;
