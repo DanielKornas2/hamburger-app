f90f23f1a817a506af02797fad8d774da1717e19
Daniel Kornas <harvi_pl@wp.pl>
2019-02-01 12:09:59 +0100

    BuildControl component to control indegredients state, parent component and styles

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,7 +5,9 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
 
+    // ingredients - it's object, not array, so i use object keys..
     const transformedIngredients = Object.keys(props.ingredients)
+    //then map and return new array , finally - return BurgerIngredient component based on state values
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient type={igKey} key={igKey + i} />;
@@ -13,6 +15,7 @@ const Burger = (props) => {
         });
 
     return (
+        //burger types are defined in BurgerIngredient component - switch statement
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
             {transformedIngredients}
